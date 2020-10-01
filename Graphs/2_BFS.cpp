2aa6005f60c7561e2d5b65725f53e4f2116c3d0e
Ishan Arya <ishanarya0@gmail.com>
2020-10-02 03:13:58 +0530

    Update 2_BFS.cpp

--- Graphs/2_BFS.cpp
+++ Graphs/2_BFS.cpp
@@ -20,15 +20,7 @@ int main() {
         for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
         cout << endl;
     }
-}// } Driver Code Ends
-
-
-/* You have to complete this function*/
-
-/* Function to do BFS of graph
-*  g[]: adj list of the graph
-*  N : number of vertices
-*/
+}
 vector <int> bfs(vector<int> g[], int N) {
     vector<int> res;
     queue<int> q; // stores the next node to visit
